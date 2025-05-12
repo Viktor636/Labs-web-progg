4c5a6031552bf45823d4c4761fab399cb077575e
Виктор Гайдабура <saxarer@mail.ru>
2025-05-12 18:57:26 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао де цзин. Канон о Дао и де',
+        publicationYear: 2022,
+        price: 850,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: 1200,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: 950,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 2023,
+        price: 650,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Рэй Брэдбери',
+        name: '451° по Фаренгейту',
+        publicationYear: 2023,
+        price: 700,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Фрэнсис Скотт Фицджеральд',
+        name: 'Великий Гэтсби',
+        publicationYear: 2022,
+        price: 550,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 2021,
+        price: 800,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Эрих Мария Ремарк',
+        name: 'Три товарища',
+        publicationYear: 2023,
+        price: 750,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Джером Д. Сэлинджер',
+        name: 'Над пропастью во ржи',
+        publicationYear: 2022,
+        price: 680,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Габриэль Гарсиа Маркес',
+        name: 'Сто лет одиночества',
+        publicationYear: 2024,
+        price: 890,
+        bestseller: true,
+        inStock: true
+    }
+];
